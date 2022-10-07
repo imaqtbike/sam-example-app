cfceff3142374f35810504c90e5cf45d77400c14
Cuong Nguyen <cuong.nguyenmanh@eightcap.com>
2022-10-07 11:03:42 +0700

    2nd

--- commands.sh
+++ commands.sh
@@ -1,3 +1,10 @@
 #create an s3 bucket
+aws s3 mb s3://cuong-code-sam
 
-aws s3 mb
+
+#package cloudformation === # sam package ....
+aws cloudformation package --s3-bucket cuong-code-sam --template-file template.yaml --output-template-file gen/template-generated.yaml
+
+#deploy
+
+aws cloudformation deploy --template-file gen\template-generated.yaml --stack-name cuong-code-sam --capabilities CAPABILITY_IAM 
--- src/app.py
+++ src/app.py
@@ -1,8 +1,21 @@
-
+from __future__ import print_function
+import boto3
 import json
 
 print('Loading function')
+dynamo = boto3.client('dynamodb')
+
+
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
 
 
 def lambda_handler(event, context):
-    return "Hello World"
+    print("Received event: " + json.dumps(event, indent=2))
+    return respond(None, res="hello world!")
